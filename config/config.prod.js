26fe13b2a87f8830766e620f2b58577b282e4c7d
许百富 <1660866966@qq.com>
2018-12-14 09:36:53 +0800

    fix

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,6 +1,36 @@
 'use strict';
-
+const variable = require('./magic_variable');
 module.exports = {
+  middleware: [ 'errorHandler', 'auth', 'aop' ], // add your config here
+  auth: {
+    ignore: variable.ignoreRouters,
+  },
+  aop: {
+    enable: false,
+    match: variable.matchAops,
+  },
+  mysql: {
+    client: {
+      host: '111.230.151.93',
+      port: '3306',
+      user: 'root',
+      password: 'fuck.646',
+      database: 'campus-plus',
+    },
+  },
+  security: {
+    csrf: false,
+  },
+  joi: {
+    options: {},
+    locale: {
+      'zh-cn': {},
+    },
+    throw: true, // when capture exception throw immediately
+    // throwHandle: (error) => { return error; }, // when throw is true the error message format
+    // errorHandle: (error) => { return error; }, // when throw is false the error message format
+    // resultHandle: (result) => { return result; } // fromat result
+  },
   cluster: {
     listen: {
       port: 7001,
